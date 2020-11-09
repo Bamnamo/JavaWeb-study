5ac542bcd34e5d48bb83ebb8f5bf9eb9333a3282
박성범 <shivash@naver.com>
2020-11-09 15:33:30 +0900

    29.5 REST 방식으로 URI 표현하기

--- pro29/src/main/java/com/myspring/pro29/HomeController.java
+++ pro29/src/main/java/com/myspring/pro29/HomeController.java
@@ -38,7 +38,7 @@ public class HomeController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-	  return "JSONTest";
+	  return "JSONTest2";
 	}
 }
 
